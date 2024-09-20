a8e4ea93084c0d2d7b71274c3fcb07f0030a7194
Matt Johnson <mattajohnson26@gmail.com>
2024-09-19 23:12:44 -0600

    fixed elements in projects and about

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -13,7 +13,7 @@ export default function Projects() {
             <div className="card-body">
               <h4 className="card-title">Park Explorer </h4>
               <h6>Contributors: Danielle Boenisch, Andrea Fuentes, Savannah Marshall</h6>
-              <a href="https://trip-planner-1cxc.onrender.com/login">
+              <a href="https://trip-planner-1cxc.onrender.com/login" target="_blank">
                 <img src={tripPlanner} width="100%"alt="first project" className="btn btn-primary"></img>
               </a>
             </div>
@@ -23,7 +23,7 @@ export default function Projects() {
           <div className="card">
             <div className="card-body">
               <h4 className="card-title">Weather Dashboard </h4>
-              <a href="https://mattaj26.github.io/Weather-Dashboard/"><img src={weatherDash} width="100%" alt="second project" className="btn btn-primary"></img>
+              <a href="https://mattaj26.github.io/Weather-Dashboard/" target="_blank"><img src={weatherDash} width="100%" alt="second project" className="btn btn-primary"></img>
               </a>
             </div>
           </div>
@@ -32,7 +32,7 @@ export default function Projects() {
           <div className="card">
             <div className="card-body">
               <h4 className="card-title">JATE: Just Another Text Editor </h4>
-              <a href="https://just-another-text-editor-aivj.onrender.com/"><img src={jateImg} width="100%" alt="third project" className="btn btn-primary"></img>
+              <a href="https://just-another-text-editor-aivj.onrender.com/" target="_blank"><img src={jateImg} width="100%" alt="third project" className="btn btn-primary"></img>
               </a>
             </div>
           </div>
